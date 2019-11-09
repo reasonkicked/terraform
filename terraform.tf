d81c0b6533277f45bcb86d3f0de7fee22f452b0c
Tomasz Stanislawczyk (PGS Software) <tstanislawczyk@pgs-soft.com>
2019-11-09 14:27:19 +0100

    test5

--- terraform.tf
+++ terraform.tf
@@ -24,8 +24,27 @@ resource "aws_instance" "example" {
 
   ami           = "ami-02df9ea15c1778c9c"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id] #<PROVIDER>_<TYPE>.<NAME>.<ATTRIBUTE>
+
+    user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello, World" > index.html
+    nohup busybox httpd -f -p 8080 &
+    EOF
 
   tags = {
-    Name = "terraform-example-zzz"
+    Name = "terraform-example"
   }
 }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
