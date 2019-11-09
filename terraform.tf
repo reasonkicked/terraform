77520d8ed2abb4a454485e9f3f5b845c35b2fc18
Tomasz Stanislawczyk (PGS Software) <tstanislawczyk@pgs-soft.com>
2019-11-09 15:27:30 +0100

    test7

--- terraform.tf
+++ terraform.tf
@@ -58,6 +58,7 @@ output "public_ip" {
   description = "The public IP address of the web server"
 }
 
+
 /*
 output "<NAME>" {
   value = <VALUE>
@@ -117,3 +118,28 @@ variable "object_example" {
   }
 }
 */
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-02df9ea15c1778c9c"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
