2ebdd5edb2f1bdf368f94a7c0c9c273108b3ca0b
Tomasz Stanislawczyk (PGS Software) <tstanislawczyk@pgs-soft.com>
2019-11-09 15:40:17 +0100

    test9

--- terraform.tf
+++ terraform.tf
@@ -151,13 +151,16 @@ resource "aws_launch_configuration" "example" {
   # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
   lifecycle {
     create_before_destroy = true
+  }
 }
-data "aws_vpc" "default" {
-  default = true
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
 }
+
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier  = data.aws_subnet_ids.default.ids #wyciagniecie id subnetow
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
   min_size = 2
   max_size = 10
 
