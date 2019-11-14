e56568ce6db001d9dfcb703e06faf93f8f8e124e
Tomasz Stanislawczyk (PGS Software) <tstanislawczyk@pgs-soft.com>
2019-11-14 10:13:47 +0100

    test34

---
+++ workspaces/main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "example" {
+  ami           = "ami-02df9ea15c1778c9c"
+  instance_type = "t2.micro"
+}
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-up-and-running-state-ts-pgs"
+    key            = "workspaces-example/terraform.tfstate"
+    region         = "eu-west-1"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks-ts-pgs"
+    encrypt        = true
+  }
+}
