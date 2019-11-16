0ffb1a9e3e8c159302d455043732bfb6dd9055b6
Tomasz Stanislawczyk (PGS Software) <tstanislawczyk@pgs-soft.com>
2019-11-16 18:09:53 +0100

    12

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -46,7 +46,7 @@ resource "aws_launch_configuration" "example" {
   image_id        = "ami-0be057a22c63962cb"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instance.id]
-
+  user_data       = data.template_file.user_data.rendered
  
   # Required when using a launch configuration with an auto scaling group.
   # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
