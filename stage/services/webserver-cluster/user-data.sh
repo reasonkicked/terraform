38b39ffe45b60c8bf38ad651f02f5b86df320b95
Tomasz Stanislawczyk (PGS Software) <tstanislawczyk@pgs-soft.com>
2019-11-16 18:11:41 +0100

    13

--- stage/services/webserver-cluster/user-data.sh
+++ stage/services/webserver-cluster/user-data.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 cat > index.html <<EOF
-    echo "Hello, World" > index.html
-    echo "${data.terraform_remote_state.db.outputs.address}" >> index.html
-    echo "${data.terraform_remote_state.db.outputs.port}" >> index.html
-    EOF
-    nohup busybox httpd -f -p ${var.server_port} &
-   
+ cat > index.html <<EOF
+<h1>Hello, World</h1>
+<p>DB address: ${db_address}</p>
+<p>DB port: ${db_port}</p>
+EOF
+
+nohup busybox httpd -f -p ${server_port} &
