2bf66cf699de5f14d41a64fbbd6287acc7d7843b
Tomasz Stanislawczyk (PGS Software) <tstanislawczyk@pgs-soft.com>
2019-11-16 17:11:18 +0100

    10

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -10,8 +10,8 @@ resource "aws_instance" "example" {
     user_data = <<-EOF
     #!/bin/bash
     echo "Hello, World" > index.html
-    echo "${data.terraform_remote_state.db.outputs.address}" >> index.html
-    echo "${data.terraform_remote_state.db.outputs.port}" >> index.html
+    /*echo "${data.terraform_remote_state.db.outputs.address}" >> index.html
+    echo "${data.terraform_remote_state.db.outputs.port}" >> index.html*/
     nohup busybox httpd -f -p ${var.server_port} &
     EOF
 
