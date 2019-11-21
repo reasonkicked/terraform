c8d12bc360ef975eb5338f615a9797f7f51d0125
Tomasz Stanislawczyk (PGS Software) <tstanislawczyk@pgs-soft.com>
2019-11-21 10:31:44 +0100

    3

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -76,10 +76,17 @@ resource "aws_lb" "example" {
   subnets            = data.aws_subnet_ids.default.ids
   security_groups    = [aws_security_group.alb.id] #security group below
 }
+locals {
+    http_port    = 80
+    any_port     = 0
+    any_protocol = "-1"
+    tcp_protocol = "tcp"
+    all_ips      = ["0.0.0.0/0"]
+    }
 #listener listening on p80 and sending 404 as default response
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
-  port              = 80
+  port              = local.http_port
   protocol          = "HTTP"
 
   # By default, return a simple 404 page
@@ -93,33 +100,30 @@ resource "aws_lb_listener" "http" {
     }
   }
 }
+
 resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-alb"
-
-  # Allow inbound HTTP requests
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow all outbound requests
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
 }
 
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
+}
 #target group
 resource "aws_lb_target_group" "asg" {
   name     = "terraform-asg-example"
--- modules/services/webserver-cluster/variables.tf
+++ modules/services/webserver-cluster/variables.tf
@@ -1,7 +1,7 @@
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
-  default     = 8080
+  default     = 80
 }
 
 variable "cluster_name" {
