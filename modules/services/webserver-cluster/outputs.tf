43a01f5c417d376d3be8d29dfd5cabb029bc6dfc
Tomasz Stanislawczyk (PGS Software) <tstanislawczyk@pgs-soft.com>
2019-11-21 12:30:02 +0100

    5

--- modules/services/webserver-cluster/outputs.tf
+++ modules/services/webserver-cluster/outputs.tf
@@ -5,7 +5,7 @@ output "public_ip" {
 
 
 output "alb_dns_name" {
-  value       = module.webserver_cluster.dns_name
+  value       = aws_lb.example.dns_name
   description = "The domain name of the load balancer"
 }
 
