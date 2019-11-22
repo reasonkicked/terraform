3455cc3b3aae0cd74a08169a21ef1d79ae68ac43
Tomasz Stanislawczyk (PGS Software) <tstanislawczyk@pgs-soft.com>
2019-11-22 13:20:06 +0100

    15

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -8,10 +8,10 @@ module "webserver_cluster" {
   cluster_name = "webservers-prod"
   db_remote_state_bucket = "terraform-up-and-running-state-ts-pgs"
   db_remote_state_key = "prod/data-stores/mysql/terraform.tfstate"
-instance_type = "t2.micro"
+  instance_type = "t2.micro"
   min_size = 2
   max_size = 10
-
+  db_location = "tsdbprod"
 }
 
 resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
