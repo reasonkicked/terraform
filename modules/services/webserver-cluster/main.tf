8190319f796120faffdcc5fbd3683026d5e6bb90
Tomasz Stanislawczyk (PGS Software) <tstanislawczyk@pgs-soft.com>
2019-11-22 13:36:42 +0100

    16

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -15,40 +15,10 @@ data "template_file" "user_data" {
 
   vars = {
     server_port = var.server_port
-    db_address  = var.db_location
-    db_port     = 3306
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
   }
 }
-
-resource "aws_dynamodb_table" "${var.db_location}" {
-  name           = "GameScores"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "UserId"
-  range_key      = "GameTitle"
-
-  attribute {
-    name = "UserId"
-    type = "S"
-  }
-
-    global_secondary_index {
-    name               = "GameTitleIndex"
-    hash_key           = "GameTitle"
-    range_key          = "TopScore"
-    write_capacity     = 10
-    read_capacity      = 10
-    projection_type    = "INCLUDE"
-    non_key_attributes = ["UserId"]
-  }
-
-  tags = {
-    Name        = "${var.db_location}"
-    Environment = "${var.cluster_name}"
-  }
-}
-
 
 resource "aws_security_group" "instance" {
   name = "${var.cluster_name}-instance"
