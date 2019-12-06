cdb4d2993a285625ec9d22124dd85a1a7e108ac7
Tomasz Stanislawczyk (PGS Software) <tstanislawczyk@pgs-soft.com>
2019-12-06 14:50:35 +0100

    2

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 # Create EC2 instance
 resource "aws_instance" "default" {
   ami                    = "var.ami"
-  count                  = "var.count"
+  count                  = 2
   key_name               = "var.key_name"
   vpc_security_group_ids = ["aws_security_group.default.id"]
   source_dest_check      = false
